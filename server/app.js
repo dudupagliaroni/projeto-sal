68e04d9e1af0dd1d6c1ea6299bf56a57854cfd5c
Eduardo Pagliaroni <dudupagliaroni@gmail.com>
2023-05-28 22:42:20 -0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use((req, res,next ) => {
+    console.log("testanto")
+    next();
+})
+
+app.use((req, res) => {
+ res.send("estou bem")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
