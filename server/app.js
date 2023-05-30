07a18090f5fb951583a1440d7ed77fda1055e159
Eduardo Pagliaroni <eduardo@codebit.com.br>
2023-05-30 13:14:52 -0300

    update configs

--- server/app.js
+++ server/app.js
@@ -2,17 +2,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/user', (req, res) => {
-
+app.get('/users', (req, res) => {
+    res.send("Usuarios")
 });
 
-app.get('/conteudo', (req, res) => {
+app.get('/users/:id', (req, res) => {
+    res.send("Usuario: " + req.params.id)
+});
 
+app.get('/conteudos', (req, res) => {
+    res.send("Conteudos")
+});
+app.get('/conteudos/:di', (req, res) => {
+    res.send("Conteudo:" + req.params.id)
 });
 
 app.get('/autores', (req, res) => {
-
+    res.send("Autores")
 });
+
+app.get('/autores/:id', (req, res) => {
+    res.send("Autor:" + req.params.id)
+});
+
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
 })
